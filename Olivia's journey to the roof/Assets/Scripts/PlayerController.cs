ac96be64f0e4ac755b897d567a85a174ce8ca7ec
Guilherme Schüler <guilherme.gschuler@gmail.com>
2021-11-17 21:45:20 -0300

    Olivia Animations
    -- finished all movement related animations for character "Olívia" and corresponding state machine.

--- Olivia's journey to the roof/Assets/Scripts/PlayerController.cs
+++ Olivia's journey to the roof/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D bc2d;
     private SpriteRenderer sr;
+    private Animator animator;
     private LayerMask unJumpable;
     private float extraHeight = .1f;
+    private float idleTimer;
 
     [SerializeField] float speed = 8f;
     [SerializeField] float fallMultiplier = 4f;
     [SerializeField] float lowJumptMultiplier = 2f;
     [SerializeField] float jumpForce = 8f;
+    [SerializeField] private float timeToSitTimer = 4f;
 
 
     // Start is called before the first frame update
@@ -23,6 +26,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         bc2d = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         unJumpable = LayerMask.GetMask("Platform"); // player can't jump from out of this layer's objects.
     }
 
@@ -35,13 +39,17 @@ public class PlayerController : MonoBehaviour
         Walk(dir);
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            animator.SetTrigger("timeToJump");
             Jump();
         }
         Fall();
+        OnLanding();
     }
 
     private void Walk(Vector2 dir) // set walking speed.
     {
+        IdleStateMachine();
+        RunStateMachine();
         Vector3 characterScale = transform.localScale;
         rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
         if (rb.velocity.x < 0 )
@@ -55,8 +63,10 @@ public class PlayerController : MonoBehaviour
         characterScale = transform.localScale;
     }
 
+
     private void Jump() // set jump force onto player. Falsifies 'isGrounded'.
     {
+        
         rb.velocity  = new Vector2(rb.velocity.x, 0);
         rb.velocity += Vector2.up * jumpForce;
     }
@@ -65,6 +75,8 @@ public class PlayerController : MonoBehaviour
     {
         if (rb.velocity.y < 0)
         {
+            Debug.Log("Fall!");
+            animator.SetTrigger("isFalling");
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
         else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
@@ -73,10 +85,49 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
         RaycastHit2D groundBoxCastInfo =  Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.down, extraHeight, unJumpable);
         return groundBoxCastInfo.collider != null;
+    }
+
+    private void OnLanding()
+    {
+        if (IsGrounded())
+        {
+            animator.SetTrigger("haveFallen");
+        }
+    }
+
+
+
+    private void RunStateMachine()
+    {
+
+        if (Input.GetAxis("Horizontal") != 0 && rb.velocity.y == 0)
+        {
+
+            animator.SetBool("timeToRun", true);
+        }
+        else if (Input.GetAxis("Horizontal") == 0 || rb.velocity.y != 0)
+        {
+
+            animator.SetBool("timeToRun", false);
+        }
+    }
+    private void IdleStateMachine()
+    {
+        idleTimer += Time.deltaTime;
+        if (Input.anyKeyDown)
+        {
+            idleTimer = 0;
+            animator.SetBool("timeToSit", false);
+        }
+        else if (idleTimer > timeToSitTimer)
+        {
+            animator.SetBool("timeToSit", true);
+        }
+
     }
 
 }
